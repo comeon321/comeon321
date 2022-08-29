ee35f3b07415bb288c1a9ff6436a1599593074b4
无言rAIn <97524199+comeon321@users.noreply.github.com>
2022-08-29 11:47:46 +0800

    Create README.cpp
    My self introduction. 
    It may have a few bugs. (smile)
    You can edit the code if you find a bug. But don't add strange things to it! You would get a fine for 100,000 even 90,000 dollars (it's fake)

---
+++ README.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+//If you can't read c++, copy the code and use online IDE. It wouldn't be pleasant, but... In Chinese, we may say "聊胜于无". 
+void wait {
+  string a;
+  cin>>a;
+}
+int main() {
+  cout<<"In this programe, you should input something after every output. If you got my meaning, then please do as I say for the body part. "<<endl;
+  wait;
+  cout<<"Hey, I'm 无言rAIn, you may call me rAIn if you can't read Chinese. "<<endl;
+  wait;
+  cout<<"I'm from China, but I don't think that it would matter- we only chat and contribute here, and we wouldn't have a battle here, right? "<<endl;
+  wait;
+  cout<<"So, what's your name?"<<endl;
+  string your_name;
+  cin>>your_name;
+  cout<<"Nice to meet you, "<<your_name<<". You see, the program is written in C++, and I use C++ most. But I'm still learning C++, and at the same time, I'm learning JS and Py. "<<endl;
+  wait;
+  cout<<"I don't think this is good for my skill. Do you agree, "<<your_name<<"?"<<endl<<"Y / N"<<endl;
+  char agreement;
+  cin>>agreement;
+  if (agreement == 'Y') cout<<"I'm trying to put off learning Python, for it's the least practical among the three. ";
+  else cout<<"Alright, being a multi-learner (?) would also be okay, but I don't think my brain can hold it!";
+  cout<<endl<<"I love music, especially funk and rock. More especially, neaural funk. And I love music games. Then you see why I like such a non-mainstream usic style!"<<endl;
+  wait;
+  cout<<"If you happened to have the same hobby as me, please go to my homepage on bilibili. I'm glad to see you there and you may send a message to me! Of course that needs an account, though."<<endl;
+  wait;
+  cout<<"I even once tryed to turn my hobby into my career - I'm a composer and a... Hmm, what would you call it? Chart writer? It's people who write charts for music games. Please tell me. ";
+  string chart_writer;
+  cin>>chart_writer;
+  cout<<"そうだよ！Thanks, "<<your_name<<"! "<<endl;
+  wait;
+  cout<<"I'm a student in junior high school, so I wouldn't (couldn't) be online frequently. Sorry for that! QwQ"<<endl;
+  cout<<"Bye and have fun!";
+  return 0;
+}
